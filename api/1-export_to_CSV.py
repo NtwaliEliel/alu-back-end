f814f888b203e7e583375a6bc9e180594195fb19
Diverse2 <ntwalie83@gamil.com>
2024-06-28 12:56:05 +0200

    1 F

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -90,7 +90,7 @@ if __name__ == '__main__':
         result = export_tasks_to_csv(Employee_Id, employee_Name, TODOS)
 
         if result == "OK":
-            sys.exit("Number of tasks in CSV: OK")  # Exit with status code 0 indicating success
+            sys.exit("Number of tasks in CSV: OK")
     else:
         print("Export failed.")
         sys.exit(1)  # Exit with status code 1 indicating failure
